b99abb72865bd6e8528561894903e75ae6b26bf9
Damien Tan <tyxdamien@Damiens-MacBook-Air.local>
2023-09-04 02:05:58 +0800

    add HeadingOne Component

---
+++ frontend/src/components/common/headings/HeadingOne.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+    Typography,
+    TypographyProps,
+    ButtonProps,
+    Grid,
+    Box,
+} from "@mui/material";
+
+type ColorType = "mono" | ButtonProps["color"];
+
+interface HeadingOneProps extends TypographyProps {
+    children: string;
+    colorScheme?: ColorType;
+}
+
+/**
+ * This is a HeadingOne custom component
+ * @property {ColorType} colorScheme
+ */
+const HeadingOne: React.FC<HeadingOneProps> = ({
+    colorScheme,
+    children,
+    ...props
+}) => {
+    const isMonoScheme = colorScheme === "mono";
+    colorScheme = colorScheme ? colorScheme : "primary";
+
+    return (
+        <Grid container xs={12}>
+            <Grid
+                item
+                xs={0.1}
+                marginRight={"1.25rem"}
+            >
+                <Box
+                    sx={{
+                        backgroundColor: isMonoScheme ? "white" : `${colorScheme}.light`,
+                        width: "0.3125rem",
+                        height: "3.625rem",
+                    }}
+                />
+
+            </Grid>
+            <Grid item xs={11}>
+                <Typography
+                    textAlign={"left"}
+                    fontFamily={""}
+                    fontSize={"3rem"}
+                    fontWeight={"400"}
+                    color={isMonoScheme ? "white" : "black"}
+                    textTransform={"uppercase"}
+                    {...props}
+                    variant="h1"
+                >
+                    {children}
+                </Typography>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default HeadingOne;
