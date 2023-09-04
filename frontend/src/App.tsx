5abae005d674a0bc3a22487b04a88473ba867c26
Bk49 <ericngyongwei@gmail.com>
2023-09-04 11:48:02 +0800

    Resolve App.tsx merge conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,3 @@
-<<<<<<< Updated upstream
-const App = () => {
-    return <></>;
-};
-=======
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -10,7 +5,6 @@ import HeadingOne from "./components/common/headings/HeadingOne";
 import HeadingTwo from "./components/common/headings/HeadingTwo";
 import HeadingThree from "./components/common/headings/HeadingThree";
 import { Grid } from "@mui/material";
-import "./App.css";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -47,22 +41,8 @@ function App() {
             <div style={{ backgroundColor: "black" }}>
                 <HeadingThree colorScheme="mono"> Heading 3 </HeadingThree>
             </div>
-
-
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
->>>>>>> Stashed changes
 
 export default App;
