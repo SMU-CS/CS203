0aef120353d3b0b97396fb23fdbbf0a384be965c
Damien Tan <tyxdamien@Damiens-MacBook-Air.local>
2023-09-09 15:19:05 +0800

    update main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import router from "./router/config"
+import router from "./router/router"
 import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
