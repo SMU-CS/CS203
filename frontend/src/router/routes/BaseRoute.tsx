2b8534dd88adc7d5ceb8ebc0294f522a6976f4ab
Damien Tan <tyxdamien@Damiens-MacBook-Air.local>
2023-09-09 15:37:24 +0800

    created router

---
+++ frontend/src/router/routes/BaseRoute.tsx
@@ -0,0 +1,52 @@
+// import { Fragment } from "react";
+// // import { SnackbarProvider } from "notistack";
+// // import ErrorMessage from "../../components/common/message/ErrorMessage";
+// // import InformationMessage from "../../components/common/message/InformationMessage";
+// // import SuccessMessage from "../../components/common/message/SuccessMessage";
+// import { useEffect } from "react";
+// // import {
+// //     queueError,
+// //     queueInfo,
+// //     queueSuccess,
+// // } from "../../functions/formHandling";
+// import { useLocation } from "react-router-dom";
+// // import { enqueueSnackbar } from "notistack";
+
+// const BaseRoute = ({ children }) => {
+//     const location = useLocation();
+
+//     // useEffect(() => {
+//     //     if (location.state) {
+//     //         const { error, info, success } = location.state;
+//     //         if (error) {
+//     //             queueError(error, enqueueSnackbar);
+//     //             delete location.state.error;
+//     //         }
+//     //         if (info) {
+//     //             queueInfo(info, enqueueSnackbar);
+//     //             delete location.state.info;
+//     //         }
+//     //         if (success) {
+//     //             queueSuccess(success, enqueueSnackbar);
+//     //             delete location.state.success;
+//     //         }
+//     //     }
+//     // }, [location]);
+
+//     return (
+//         <Fragment>
+//             <SnackbarProvider
+//                 maxSnack={5}
+//                 Components={{
+//                     customError: ErrorMessage,
+//                     customInfo: InformationMessage,
+//                     customSuccess: SuccessMessage,
+//                 }}
+//             >
+//                 {children}
+//             </SnackbarProvider>
+//         </Fragment>
+//     );
+// };
+
+// export default BaseRoute;
---
+++ frontend/src/router/routes/PublicRoute.tsx
@@ -0,0 +1,40 @@
+import NavBar from "../../components/common/navigations/NavBar";
+//import BaseRoute from "./BaseRoute";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+// import { queueSuccess } from "../../functions/formHandling";
+// import { enqueueSnackbar } from "notistack";
+// import jwtDecode from "jwt-decode";
+// import Cookies from "js-cookie";
+
+const PublicRoute = ({ children }) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // const token = Cookies.get("token");
+
+        // if (token) {
+        //     try {
+        //         const {sub, exp} = jwtDecode(token)
+        //         if(sub.length > 0 && new Date(exp * 1000) > new Date()){
+        //             return navigate("/my-summary", {state: {info: "Session found, redirect to Summary Page"}})
+        //         }
+        //     } catch (e) {
+        //         queueSuccess("Invalid/ Expired token found, removing token", enqueueSnackbar)
+        //         Cookies.set("token", null);
+        //     }
+        // }
+    }, [navigate]);
+
+    return (
+        //<BaseRoute>
+        <div>
+            <NavBar authenticated="false" />
+            <div style={{ margin: "1.5rem" }}>{children}</div>
+        </div>
+            
+        //</BaseRoute>
+    );
+};
+
+export default PublicRoute;
