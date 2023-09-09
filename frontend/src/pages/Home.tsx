02def602a9869322e39d4bb37141af479d57eff7
Damien Tan <tyxdamien@Damiens-MacBook-Air.local>
2023-09-09 15:37:01 +0800

    created pages

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/common/Footer";
+import HeadingOne from "../components/common/headings/HeadingOne";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Footer></Footer>
+            <HeadingOne colorScheme="primary" > Testing Heading One </HeadingOne>
+        </div>
+    )
+
+}
+export default Home;
