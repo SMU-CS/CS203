6debdbe5b6f72fbcd26a2c52bc3b2a9d7f43a41b
Bk49 <ericngyongwei@gmail.com>
2023-09-12 00:06:04 +0800

    Fix Button component

--- frontend/src/components/common/buttons/Button.tsx
+++ frontend/src/components/common/buttons/Button.tsx
@@ -1,59 +1,21 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import {
     Button as MuiButton,
     ButtonProps as MuiButtonProps,
 } from "@mui/material";
 
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-
 interface ButtonProps extends MuiButtonProps {
     children: string;
-    colorType: 'primary' | 'secondary';
-    hasNextIcon: boolean;
-    variantType: 'outlined' | 'contained';
 }
 
 /**
- * This is a button that our team create, it takes in mainly
+ * Common Button component used in the application. It can take in all the properties listed out by MUI Button API
  * @param {ReactChildren} children
  *
+ * @returns {ReactElement} Button
  */
-const Button: React.FC<ButtonProps> = ({ children, colorType, hasNextIcon, variantType, ...props }) => {
-
-    const isContained = variantType === "contained";
-    const nextIcon = hasNextIcon ? <ArrowForwardIcon /> : null;
-
-    return (
-        <MuiButton
-
-            variant= {variantType}
-            endIcon= {nextIcon}
-            
-
-            sx={{
-                
-                backgroundColor: isContained ? `${colorType}.main` : 'transparent',
-                borderColor: isContained ? 'transparent' : `${colorType}.main`,
-                boxShadow: isContained ? 4 : 0,
-                color: isContained ? "white" : `${colorType}.main`,
-                margin: "5px",
-                paddingLeft: "25px",
-                paddingRight: "25px",
-                
-                ":hover": {
-                    backgroundColor: isContained ? `${colorType}.dark` : 'transparent',
-                    borderColor: isContained ? 'transparent' : `${colorType}.dark`,
-                    boxShadow: 4,
-                    color: isContained ? "white" : `${colorType}.dark`,
-                },
-
-            }} 
-            
-            {...props}
-        >
-            {children}
-        </MuiButton>
-    );
+const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+    return <MuiButton {...props}>{children}</MuiButton>;
 };
 
 export default Button;
