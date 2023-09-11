eed97b47aa11b7b7be9c8986faf1266674e09a87
Bk49 <ericngyongwei@gmail.com>
2023-09-11 22:50:26 +0800

    Import Typography in App.tsx and test the components out

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,75 @@
-import HeadingOne from "./components/common/headings/HeadingOne";
-import HeadingTwo from "./components/common/headings/HeadingTwo";
-import HeadingThree from "./components/common/headings/HeadingThree";
+import Heading from "./components/common/headings/Heading";
 import Button from "./components/common/buttons/Button";
+import { Typography } from "@mui/material";
 
 const App = () => {
     return (
         <>
-
-            <HeadingOne colorScheme="primary"> Heading 1 </HeadingOne>
+            <Heading color="primary"> Heading 1 </Heading>
             <h1></h1>
 
-            <HeadingTwo colorScheme="primary"> Heading 2 </HeadingTwo>
+            <Heading variant="h2" color="primary">
+                Heading 2
+            </Heading>
             <h1></h1>
 
-            <HeadingThree colorScheme="primary"> Heading 3 </HeadingThree>
+            <Heading variant="h3" color="primary">
+                Heading 3
+            </Heading>
             <h1></h1>
-            
+
             <div style={{ backgroundColor: "black" }}>
-                <HeadingOne colorScheme="mono"> Heading 1 </HeadingOne>
+                <Heading color="mono"> Heading 1 </Heading>
             </div>
             <h1></h1>
 
             <div style={{ backgroundColor: "black" }}>
-                <HeadingTwo colorScheme="mono"> Heading 2 </HeadingTwo>
+                <Heading variant="h2" color="mono">
+                    Heading 2
+                </Heading>
             </div>
             <h1></h1>
 
             <div style={{ backgroundColor: "black" }}>
-                <HeadingThree colorScheme="mono"> Heading 3 </HeadingThree>
+                <Heading variant="h3" color="mono">
+                    Heading 3
+                </Heading>
             </div>
 
-            <Button colorType='secondary' variantType='contained' hasNextIcon={false}>regular</Button>
+            <Typography fontWeight="bold">Body 1 (Bold)</Typography>
+            <Typography>Body 1 (Bold)</Typography>
+            <Typography variant="body2" fontWeight="bold">
+                Body 2 (Bold)
+            </Typography>
+            <Typography variant="body2">Body 2</Typography>
+            <Typography variant="subtitle1" fontWeight="bold">
+                Subtitle 1 (Bold)
+            </Typography>
+            <Typography variant="subtitle1">Subtitle 1</Typography>
+            <Typography variant="subtitle2" fontWeight="bold">
+                Subtitle 2 (Bold)
+            </Typography>
+            <Typography variant="subtitle2">Subtitle 2 (Bold)</Typography>
 
-            <Button colorType='secondary' variantType='outlined' hasNextIcon={false}>outlined</Button>
+            <Button
+                colorType="secondary"
+                variantType="contained"
+                hasNextIcon={false}
+            >
+                regular
+            </Button>
 
-            <Button colorType='primary' variantType='contained' hasNextIcon>next</Button>
+            <Button
+                colorType="secondary"
+                variantType="outlined"
+                hasNextIcon={false}
+            >
+                outlined
+            </Button>
+
+            <Button colorType="primary" variantType="contained" hasNextIcon>
+                next
+            </Button>
         </>
     );
 };
