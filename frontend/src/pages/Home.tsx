d5bee8f73a9fa655655643f00df9f421e875769e
Bk49 <ericngyongwei@gmail.com>
2023-09-17 14:14:44 +0800

    Remove Footer from Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import Footer from "../components/common/footer/Footer";
 import Heading from "../components/common/headings/Heading";
 
 const Home = () => {
     return (
         <>
             <Heading color="primary"> Testing Heading One </Heading>
-            <Footer />
         </>
     );
 };
