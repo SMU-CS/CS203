14d723bf64a6a5d4ee6b82392f1812f3f69102b5
Bk49 <ericngyongwei@gmail.com>
2023-09-20 20:24:48 +0800

    Create interfaces for activity, event, purchaseRequest and salesRound

---
+++ frontend/src/types/salesround.ts
@@ -0,0 +1,9 @@
+export interface SalesRound {
+    sales_round_id: string;
+    activity_id: string;
+    round_start: string;
+    round_end: string;
+    purchase_start: string;
+    purchase_end: string;
+    sales_type: string;
+}
