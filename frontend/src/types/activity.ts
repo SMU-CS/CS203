3a18c66ebff119912feb8c02b62bd4cdb9f63682
Bk49 <ericngyongwei@gmail.com>
2023-09-21 23:21:58 +0800

    Add in Ticket Types

--- frontend/src/types/activity.ts
+++ frontend/src/types/activity.ts
@@ -1,10 +1,12 @@
 import { SalesRound } from "./salesround";
+import { TicketType } from "./ticket";
 
 export interface Activity {
     activity_id: string;
     event_id: string;
-    start_datetime:string;
-    end_datetime:string;
+    start_datetime: string;
+    end_datetime: string;
     location: string;
     sales_rounds?: SalesRound[];
+    ticket_types?: TicketType[];
 }
---
+++ frontend/src/types/ticket.ts
@@ -0,0 +1,7 @@
+export interface TicketType {
+    ticket_type_id: string;
+    activity_id: string;
+    type: string;
+    price: number;
+    description: string;
+}
