cb03944cb93d95c8ebadab45caf02d0032cc2bfc
Bk49 <ericngyongwei@gmail.com>
2023-09-24 21:35:19 +0800

    Add in API call for getting event details

--- frontend/src/axios/event/event.ts
+++ frontend/src/axios/event/event.ts
@@ -1,6 +1,10 @@
 import { eventInstance } from "../instance";
-import { EventListingType } from "../../types/event";
-import { formatDateToDateWithDay } from "../../functions/formatter";
+import { EventDetailsType, EventListingType } from "../../types/event";
+import {
+    formatDateToDateWithDay,
+    formatDatetoDateTimeWithDay,
+} from "../../functions/formatter";
+import { Activity } from "../../types/activity";
 
 export const listEvents = async (isFeatured?: boolean) => {
     try {
@@ -17,6 +21,35 @@ export const listEvents = async (isFeatured?: boolean) => {
                 end_datetime: formatDateToDateWithDay(new Date(end_datetime)),
             })
         ) as EventListingType[];
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const getEvent = async (id?: number | string) => {
+    try {
+        const { data } = await eventInstance.get(`/${id}`);
+
+        let event = {
+            ...data,
+            start_datetime: formatDateToDateWithDay(
+                new Date(data.start_datetime)
+            ),
+            end_datetime: formatDateToDateWithDay(new Date(data.end_datetime)),
+            activities: data.activities.map(
+                ({ startDateTime, endDateTime, ...other }: Activity) => ({
+                    ...other,
+                    startDateTime: formatDatetoDateTimeWithDay(
+                        new Date(startDateTime)
+                    ),
+                    endDateTime: formatDatetoDateTimeWithDay(
+                        new Date(endDateTime)
+                    ),
+                })
+            ),
+        };
+
+        return event as EventDetailsType;
     } catch (e) {
         console.log(e);
     }
