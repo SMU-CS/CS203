5b7edc829fdbe8b7527b65e7da8a3d165bc7089a
Bk49 <ericngyongwei@gmail.com>
2023-09-24 21:34:32 +0800

    Add format date to datetime with day function to formatter

--- frontend/src/functions/formatter.tsx
+++ frontend/src/functions/formatter.tsx
@@ -1,13 +1,13 @@
-/**
- * Takes in a string of in the format of `DD/MM/YYYY` and returns it in `DD MMM YYYY (Day)`
- * @param {string} inputDate 
- * @returns {string} date with day
- */
 export const formatDateToDateWithDay = (date: Date) => {
+    const utcDate = date.toUTCString();
+    const dayOfWeek = utcDate.substring(0, 3);
+    const dateStr = utcDate.substring(4, 16);
 
-    const utcDate = date.toUTCString()
-    const dayOfWeek = utcDate.substring(0, 3)
-    const dateStr = utcDate.substring(4, 16)
+    return `${dateStr} (${dayOfWeek}.)`;
+};
 
-    return `${dateStr} (${dayOfWeek}.)`
+export const formatDatetoDateTimeWithDay = (date: Date) => {
+    const timeStr = date.toTimeString();
+
+    return `${formatDateToDateWithDay(date)} ${timeStr.substring(0, 5)}`;
 };
