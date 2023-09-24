99ad48e93d20d39187162eaa60e14c0630a059cc
Bk49 <ericngyongwei@gmail.com>
2023-09-24 21:36:16 +0800

    Modify EventDescription to accept properties for dynamic rendering

--- frontend/src/components/event/card/EventDescription.tsx
+++ frontend/src/components/event/card/EventDescription.tsx
@@ -1,6 +1,10 @@
 import { Box, Typography } from "@mui/material";
 
-const EventDescription = () => {
+interface EventDescriptionType {
+    children?: string;
+}
+
+const EventDescription: React.FC<EventDescriptionType> = ({ children }) => {
     return (
         <Box>
             <Typography
@@ -9,18 +13,7 @@ const EventDescription = () => {
                 paddingTop={"1rem"}
                 paddingBottom={"1rem"}
             >
-                this is a text test Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Esse aliquid natus reprehenderit ducimus,
-                doloribus cumque aliquam officiis totam nisi blanditiis veniam,
-                magnam dolorum dolore. Fuga tenetur architecto incidunt ab iure?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                suscipit corporis at nesciunt necessitatibus quaerat odit?
-                Minima tenetur odio accusantium magni accusamus alias laborum
-                nulla laudantium eveniet, iste, quibusdam ipsam. Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Illum perspiciatis
-                velit deserunt assumenda veritatis. Eum sint voluptate
-                asperiores neque praesentium. Perferendis, incidunt magnam quo
-                deleniti fugit mollitia repellat excepturi ab!
+                {children}
             </Typography>
         </Box>
     );
