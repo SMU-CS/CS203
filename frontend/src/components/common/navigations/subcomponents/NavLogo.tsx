7f4fbab544bd34258b044e15a54d764b2d6630e5
Bk49 <ericngyongwei@gmail.com>
2023-09-26 11:33:13 +0800

    Modify NavLogo to re-route to home page

--- frontend/src/components/common/navigations/subcomponents/NavLogo.tsx
+++ frontend/src/components/common/navigations/subcomponents/NavLogo.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import AppLogo from "../../../../assets/app-logos/white-logo.png";
+import { useNavigate } from "react-router-dom";
 
 const NavLogo: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({
     ...props
 }) => {
+    const navigate = useNavigate();
+
     return (
         <img
             {...props}
+            onClick={() => navigate("/")}
             src={AppLogo}
-            style={{ display: "flex", width:"6rem" }}
+            style={{ display: "flex", width: "6rem" }}
             alt="App Logo"
         />
     );
