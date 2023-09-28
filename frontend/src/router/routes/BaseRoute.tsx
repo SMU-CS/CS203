64b7aa2b053a68882a3764439818ea2535319583
Bk49 <ericngyongwei@gmail.com>
2023-09-28 23:28:22 +0800

    (Comments only) Modify BaseRoute

--- frontend/src/router/routes/BaseRoute.tsx
+++ frontend/src/router/routes/BaseRoute.tsx
@@ -1,51 +1,19 @@
-// import { Fragment } from "react";
-// // import { SnackbarProvider } from "notistack";
-// // import ErrorMessage from "../../components/common/message/ErrorMessage";
-// // import InformationMessage from "../../components/common/message/InformationMessage";
-// // import SuccessMessage from "../../components/common/message/SuccessMessage";
-// import { useEffect } from "react";
-// // import {
-// //     queueError,
-// //     queueInfo,
-// //     queueSuccess,
-// // } from "../../functions/formHandling";
-// import { useLocation } from "react-router-dom";
-// // import { enqueueSnackbar } from "notistack";
+// import React, { ReactNode } from "react";
+// import { SnackbarProvider, enqueueSnackbar } from "notistack";
 
-// const BaseRoute = ({ children }) => {
-//     const location = useLocation();
+// interface BaseRouteProps {
+//     children: ReactNode;
+// }
 
-//     // useEffect(() => {
-//     //     if (location.state) {
-//     //         const { error, info, success } = location.state;
-//     //         if (error) {
-//     //             queueError(error, enqueueSnackbar);
-//     //             delete location.state.error;
-//     //         }
-//     //         if (info) {
-//     //             queueInfo(info, enqueueSnackbar);
-//     //             delete location.state.info;
-//     //         }
-//     //         if (success) {
-//     //             queueSuccess(success, enqueueSnackbar);
-//     //             delete location.state.success;
-//     //         }
-//     //     }
-//     // }, [location]);
+// const BaseRoute: React.FC<BaseRouteProps> = ({ children }) => {
+    
 
 //     return (
-//         <Fragment>
 //             <SnackbarProvider
-//                 maxSnack={5}
-//                 Components={{
-//                     customError: ErrorMessage,
-//                     customInfo: InformationMessage,
-//                     customSuccess: SuccessMessage,
-//                 }}
+//                 maxSnack={3}
 //             >
 //                 {children}
 //             </SnackbarProvider>
-//         </Fragment>
 //     );
 // };
 
