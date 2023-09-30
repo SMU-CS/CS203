283f76e15b27fe78e4c649c18cbcce76c1d9f60c
tyxdamien <115641982+tyxdamien@users.noreply.github.com>
2023-09-30 13:48:05 +0800

    created new types for creditcard and profile

---
+++ frontend/src/types/creditcard.ts
@@ -0,0 +1,6 @@
+export interface creditcard {
+    card_number: string;
+    card_name: string;
+    cvv: string;
+    expiration_date: string;
+}
---
+++ frontend/src/types/profile.ts
@@ -0,0 +1,11 @@
+export interface profile {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    contact: string;
+    date_of_birth: string;
+    country: string;
+    postal_code: string;
+    password: string;
+}
