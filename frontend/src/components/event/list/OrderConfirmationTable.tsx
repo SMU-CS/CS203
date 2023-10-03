4b9aa17feddde6f89effdb10d93868c9acfd3d04
Poh En Xi <enxi.poh.2022@scis.smu.edu.sg>
2023-10-03 19:32:04 +0800

    created OrderConfirmation page, OrderConfirmationTable component

---
+++ frontend/src/components/event/list/OrderConfirmationTable.tsx
@@ -0,0 +1,84 @@
+import { Grid, TableProps, Typography } from "@mui/material";
+import PurchaseRequestItem from "../../pr/text/PurchaseRequestItem";
+import SectionDivider from "../layout/divider/SectionDivider";
+
+interface PurchaseRequestItem {
+    ticket_type_id: string;
+    activity_id: string;
+    type: string;
+    price: number;
+    quantity: number;
+    datetime: string;
+    location: string;
+}
+
+interface OrderConfirmationTableProps extends TableProps {
+    Transactions: PurchaseRequestItem[];
+    ServiceFee: number;
+    FacilityCharge: number;
+}
+
+const OrderConfirmationTable: React.FC<OrderConfirmationTableProps> = ({
+    Transactions,
+    ServiceFee,
+    FacilityCharge,
+}) => {
+    const TotalPrice = Transactions.reduce(
+        (accumulator, purchase) => accumulator + purchase.price,
+        0
+    );
+
+    const sc = (TotalPrice / 100) * ServiceFee;
+
+    return (
+        <>
+            <Grid sx={{ my: "2rem" }}>
+                <Typography variant="body2">
+                    <Grid my={"1rem"}>
+                        {Transactions.map((purchase) => (
+                            <PurchaseRequestItem
+                                item={purchase}
+                            ></PurchaseRequestItem>
+                        ))}
+                    </Grid>
+
+                    <SectionDivider />
+
+                    <Grid my={"1rem"}>
+                        <Grid
+                            container
+                            justifyContent="space-between"
+                            sx={{ my: "1.5rem" }}
+                        >
+                            <Grid item>Total:</Grid>
+                            <Grid item>{"$" + TotalPrice.toFixed(2)}</Grid>
+                        </Grid>
+
+                        <Grid container justifyContent="space-between">
+                            <Grid item>Service Charge:</Grid>
+                            <Grid item>{"$" + sc.toFixed(2)}</Grid>
+                        </Grid>
+
+                        <Grid container justifyContent="space-between">
+                            <Grid item>Facility Charge:</Grid>
+                            <Grid item>{"$" + FacilityCharge.toFixed(2)}</Grid>
+                        </Grid>
+                    </Grid>
+
+                    <SectionDivider />
+                </Typography>
+                <Typography variant="body1" fontWeight={"bold"}>
+                    <Grid container justifyContent="space-between" my={"1rem"}>
+                        <Grid item>Total Paid:</Grid>
+                        <Grid item>
+                            {"$" +
+                                (TotalPrice + sc + FacilityCharge).toFixed(2)}
+                        </Grid>
+                    </Grid>
+                </Typography>
+            </Grid>
+        </>
+    );
+};
+
+export default OrderConfirmationTable;
---
+++ frontend/src/pages/customer/OrderConfirmation.tsx
@@ -0,0 +1,96 @@
+import { useNavigate, useParams } from "react-router-dom";
+import EventBanner from "../../components/event/banner/EventBanner";
+import { useQuery } from "@tanstack/react-query";
+import { getEvent } from "../../axios/event/event";
+import EventBannerSkeleton from "../../components/event/banner/EventBannerSkeleton";
+import { Button, Container, Grid, Typography } from "@mui/material";
+import Heading from "../../components/common/headings/Heading";
+import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
+import OrderConfirmationTable from "../../components/event/list/OrderConfirmationTable";
+
+const transaction = [
+    {
+        ticket_type_id: "1",
+        activity_id: "1",
+        type: "Standard - Cat A",
+        price: 50,
+        quantity: 1,
+        datetime: "02 Sep 2023 (Sat.) 06:00pm",
+        location: "Singapore Indoor Stadium",
+    },
+    {
+        ticket_type_id: "2",
+        activity_id: "1",
+        type: "Standard - Cat B",
+        price: 20,
+        quantity: 1,
+        datetime: "02 Sep 2023 (Sat.) 06:00pm",
+        location: "Singapore Indoor Stadium",
+    },
+];
+
+const OrderConfirmation = () => {
+
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    const { data: event } = useQuery({
+        queryKey: ["eventDetails", id],
+        queryFn: () => getEvent(id),
+    });
+
+    return (
+        <>
+            <Container maxWidth="lg">
+                <Grid container gap={7} my="3rem">
+                    {!event ? (
+                        <>
+                            <EventBannerSkeleton />
+                        </>
+                    ) : (
+                        <>
+                            <EventBanner event={event} />
+                        </>
+                    )}
+                    <Grid
+                        justifyContent={"center"}
+                        width={"50rem"}
+                        mx={"11rem"}
+                    >
+                        <Heading color="primary">Order Confirmation</Heading>
+
+                        <Grid
+                            display={"flex"}
+                            direction={"row"}
+                            sx={{ my: "2rem" }}
+                        >
+                            <CheckCircleRoundedIcon color="primary" />
+                            <Typography sx={{ mx: "0.5rem" }}>
+                                Your payment has been received, thank you for
+                                supporting EzTix!
+                            </Typography>
+                        </Grid>
+
+                        <OrderConfirmationTable
+                            Transactions={transaction}
+                            ServiceFee={10}
+                            FacilityCharge={10}
+                        ></OrderConfirmationTable>
+
+                        <Grid container direction={"row"} justifyContent={"flex-end"}>
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                onClick={() => navigate(`/event/${id}`)}
+                            >
+                                Browse Other Events
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Container>
+        </>
+    );
+};
+
+export default OrderConfirmation;
