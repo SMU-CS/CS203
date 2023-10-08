4c41aa969ee2e438000f33d636806d6b4eeaf7b3
Poh En Xi <enxi.poh.2022@scis.smu.edu.sg>
2023-10-08 23:26:51 +0800

    updated ChipPurchaseStatus, OrderCard CSS

--- frontend/src/components/event/card/OrderCard.tsx
+++ frontend/src/components/event/card/OrderCard.tsx
@@ -8,68 +8,71 @@ import {
     Button,
     CardActionArea,
     CardActions,
-    Chip,
-    useTheme,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { EventListingType } from "../../../types/event";
+import OrderChip from "../chip/OrderChip";
 
 interface OrderCardProps extends CardProps {
     event: EventListingType;
-    ChipPurchaseStatus: string;
+    ChipPurchaseStatus: "Pending" | "Processing" | "Recurring" | "Past";
     ButtonPurchaseStatus: string;
 }
 
-const CardStyles = {
-    width: { xs: "14rem", sm: "16rem", md: "18rem" },
-    height: { xs: "18rem", sm: "20rem", md: "22rem" },
-};
-
 const CardMediaStyles = {
     width: "100%",
-    height: { xs: "4rem", sm: "6rem", md: "8rem" },
+    height: { xs: "6rem", sm: "8rem", md: "10rem" },
 };
 
-const CardContentStyles = {
-    justifyContent: "space-between",
-    direction: "column",
-    height: { xs: "14rem", sm: "16rem", md: "18rem" }
-};
-
-const ChipStyles = {
-    fontFamily: "CircularSpotify",
-    fontSize: {sm:"0.5rem", md: "0.7rem", lg: "0.9rem"},
-    fontWeight: "bold"
-};
-
-const OrderCard: React.FC<OrderCardProps> = ({ event, ChipPurchaseStatus, ButtonPurchaseStatus, ...props }) => {
+const OrderCard: React.FC<OrderCardProps> = ({
+    event,
+    ChipPurchaseStatus,
+    ButtonPurchaseStatus,
+    ...props
+}) => {
     const { name, bannerURL, id } = event;
     const navigate = useNavigate();
-    const theme = useTheme();
 
-    const ChipColor = ChipPurchaseStatus === "Pending Payment" ? theme.palette.primary.dark : theme.palette.secondary.dark
-    const ButtonColor = ButtonPurchaseStatus === "Fulfil Purchase Request" ? "primary" : "secondary"
+    const ButtonColor =
+        ChipPurchaseStatus === "Pending" || "Recurring"
+            ? "primary"
+            : "secondary";
 
     return (
-        <Card {...props} sx={CardStyles}>
-            <Grid height={{ xs: "14rem", sm: "16rem", md: "18rem" }}>
-                <CardActionArea onClick={() => navigate(`/event/${id}`)}>
-                    <CardMedia sx={CardMediaStyles} image={bannerURL} />
-                    <CardContent sx={CardContentStyles}>
-                        <Chip label={ChipPurchaseStatus} color={"default"} style={{backgroundColor: ChipColor, color: "#fff"}} sx={ChipStyles} />
-                        <Grid sx={{ mt: "0.5rem" }}>
-                            <Typography variant="body1" fontWeight="bold">
-                                {name}
-                            </Typography>
+        <Card {...props}>
+            <CardActionArea onClick={() => navigate(`/event/${id}`)}>
+                <CardMedia sx={CardMediaStyles} image={bannerURL} />
+                <CardContent>
+                    <Grid
+                        container
+                        sx={{
+                            height: { xs: "120px", sm: "150px", md: "180px" },
+                        }}
+                        direction="column"
+                    >
+                        <Grid item>
+                            <OrderChip
+                                ChipPurchaseStatus={ChipPurchaseStatus}
+                            />
                         </Grid>
-                    </CardContent>
-                </CardActionArea>
+
+                        <Typography
+                            variant="body1"
+                            fontWeight="bold"
+                            my={"0.5rem"}
+                        >
+                            {name}
+                        </Typography>
+                    </Grid>
+                </CardContent>
+            </CardActionArea>
+            <Grid sx={{ ml: "0.5rem", mb: "0.5rem" }}>
+                <CardActions>
+                    <Button color={ButtonColor} variant="contained">
+                        {ButtonPurchaseStatus}
+                    </Button>
+                </CardActions>
             </Grid>
-            <CardActions sx={{ ml: "0.5rem" }}>
-                <Button color={ButtonColor} variant="contained">
-                    {ButtonPurchaseStatus}
-                </Button>
-            </CardActions>
         </Card>
     );
 };
--- frontend/src/pages/customer/OrderHistory.tsx
+++ frontend/src/pages/customer/OrderHistory.tsx
@@ -15,29 +15,34 @@ const OrderHistory = () => {
         queryFn: () => listEvents(false),
     });
 
-const labels = [
-    "Purchase Requests",
-    "Order History"
-];
+    const labels = ["Purchase Requests", "Order History"];
 
     return (
         <>
-        <TabBar labels={labels} displayBuyTicketButton={false}></TabBar>
-        <Grid container spacing={2} justifyContent={"center"}>
-            {events &&
-                events.map((details) => (
-                    <Grid
-                        sx={{
-                        margin: { xs: "1rem", md: "1.5rem", lg: "2rem"}, 
-                        }}
-                        xs={8.5}
-                        sm={4.5}
-                        lg={2.5}
-                        direction={"row"}
-                    >
-                        <OrderCard event={details} ChipPurchaseStatus={"Pending Payment"} ButtonPurchaseStatus="Fulfil Purchase Request"/>
-                    </Grid>
-                ))}
+            <TabBar labels={labels}></TabBar>
+            <Grid container spacing={2} justifyContent={"center"}>
+                {events &&
+                    events.map((details) => (
+                        <Grid
+                            sx={{
+                                margin: {
+                                    xs: "1rem",
+                                    md: "1.5rem",
+                                    lg: "2rem",
+                                },
+                            }}
+                            xs={8}
+                            sm={5}
+                            lg={3}
+                            direction={"row"}
+                        >
+                            <OrderCard
+                                event={details}
+                                ChipPurchaseStatus={"Processing"}
+                                ButtonPurchaseStatus="Fulfil Purchase Request"
+                            />
+                        </Grid>
+                    ))}
             </Grid>
         </>
     );
