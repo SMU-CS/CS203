a4267b8b492956046a3ab04667063e9bbde62d48
tyxdamien <115641982+tyxdamien@users.noreply.github.com>
2023-10-22 14:58:47 +0800

    updated TextField component

--- frontend/src/components/common/form/TextField.tsx
+++ frontend/src/components/common/form/TextField.tsx
@@ -4,12 +4,13 @@ import {
     InputAdornment,
     TextField as MuiTextField,
 } from "@mui/material";
-import { useFormContext, Controller } from "react-hook-form";
+import { useFormContext, Controller, RegisterOptions, FieldValues } from "react-hook-form";
 import { SvgIconComponent } from "@mui/icons-material";
 
 interface TextFieldProps extends BaseTextFieldProps {
     Icon?: SvgIconComponent;
     iconPosition?: "start" | "end";
+    rules?: RegisterOptions<FieldValues, string>
 }
 
 const TextField: React.FC<TextFieldProps> = ({
@@ -20,6 +21,7 @@ const TextField: React.FC<TextFieldProps> = ({
     Icon,
     iconPosition,
     sx,
+    rules,
     ...props
 }) => {
     const { control } = useFormContext();
@@ -28,6 +30,7 @@ const TextField: React.FC<TextFieldProps> = ({
         <Controller
             {...props}
             control={control}
+            rules={rules}
             name={name ? name : "text field"}
             render={({ field, fieldState }) => (
                 <MuiTextField
