020c4cd71eaddacb69b88977cae2cd1d4f525800
tyxdamien <115641982+tyxdamien@users.noreply.github.com>
2023-10-30 01:28:17 +0800

    updated calculations for PurchaseDetails to the calculation function

--- frontend/src/functions/calculation.ts
+++ frontend/src/functions/calculation.ts
@@ -1,11 +1,43 @@
 export const calcTicketPrice = (transactions: PurchaseRequestItem[]) => {
-    return transactions.reduce((accumulator, purchase) => accumulator + purchase.price, 0 )
-}
+    return transactions.reduce(
+        (accumulator, purchase) => accumulator + purchase.price,
+        0
+    );
+};
 
-export const calcServiceFee = (transactions: PurchaseRequestItem[], serviceFee: number) => {
-    return (calcTicketPrice(transactions) / 100) * serviceFee
-}
+export const calcServiceFee = (
+    transactions: PurchaseRequestItem[],
+    serviceFee: number
+) => {
+    return (calcTicketPrice(transactions) / 100) * serviceFee;
+};
 
-export const calcTotalPrice = (transactions: PurchaseRequestItem[], serviceFee: number, facilityCharge: number) => {
-    return calcTicketPrice(transactions) + calcServiceFee(transactions, serviceFee) + facilityCharge
-}
+export const calcTotalPrice = (
+    transactions: PurchaseRequestItem[],
+    serviceFee: number,
+    facilityCharge: number
+) => {
+    return (
+        calcTicketPrice(transactions) +
+        calcServiceFee(transactions, serviceFee) +
+        facilityCharge
+    );
+};
+
+export const calculateTotalPrice = (
+    purchaseRequestItems: PurchaseDetails[]
+) => {
+    return purchaseRequestItems.reduce((acc, item) => acc + item.price, 0);
+};
+
+export const calculateNettTotal = (
+    total: number,
+    serviceFee: number,
+    facilityCharge: number
+) => {
+    return total + serviceFee + facilityCharge;
+};
+
+export const calculateServiceFee = (total: number) => {
+    return (total * 10) / 100;
+};
