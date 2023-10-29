e0766aa17763e35af55971ebad850f926cd426d4
Bk49 <ericngyongwei@gmail.com>
2023-10-29 19:56:04 +0800

    Complete fulfil purchase request get data for now

--- frontend/src/components/event/table/FulfilPurchaseRequestTable.tsx
+++ frontend/src/components/event/table/FulfilPurchaseRequestTable.tsx
@@ -1,4 +1,3 @@
-import { Activity } from "../../../types/activity";
 import {
     TableContainer,
     Table,
@@ -12,12 +11,14 @@ import {
 } from "@mui/material";
 import PurchaseRequestTableHeaderCell from "./sub-components/PurchaseRequestTableHeaderCell";
 import PurchaseRequestTableRow from "./sub-components/PurchaseRequestTableRow";
+import { PurchaseRequestItemWithDetails } from "../../../types/pr";
+import { formatDateToDateWithDay } from "../../../functions/formatter";
 
 interface FulfilPurchaseRequestTableProps extends TableProps {
-    activities: Activity[];
+    prItems: PurchaseRequestItemWithDetails[];
 }
 const FulfilPurchaseRequestTable: React.FC<FulfilPurchaseRequestTableProps> = ({
-    activities,
+    prItems,
     ...props
 }) => {
     const theme = useTheme();
@@ -27,19 +28,31 @@ const FulfilPurchaseRequestTable: React.FC<FulfilPurchaseRequestTableProps> = ({
             <Table {...props}>
                 <TableHead sx={{ bgcolor: theme.palette.primary.light }}>
                     <TableRow>
-                        <TableCell/>
+                        <TableCell />
                         <PurchaseRequestTableHeaderCell></PurchaseRequestTableHeaderCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {activities.map((activity) =>
-                        activity.ticketTypes.map((ticket) => (
-                            <PurchaseRequestTableRow
-                                startDateTime={activity.startDateTime}
-                                ticketType={"cat A"}
-                                price={77}
-                            />
-                        ))
+                    {prItems.map(
+                        (
+                            {
+                                quantityRequested,
+                                ticketType,
+                                price,
+                                eventStartDateTime,
+                            },
+                            index
+                        ) =>
+                            [...Array(quantityRequested)].map((i) => (
+                                <PurchaseRequestTableRow
+                                    key={`${i}${index}`}
+                                    startDateTime={formatDateToDateWithDay(
+                                        new Date(eventStartDateTime)
+                                    )}
+                                    ticketType={ticketType}
+                                    price={price / quantityRequested}
+                                />
+                            ))
                     )}
                 </TableBody>
             </Table>
